abb68f17808d4341dec8ffc4bc0ebeaf0996dc27
pendecarcoding <bohatimulyadi99@gmail.com>
2022-10-23 09:15:36 +0700

    new file:   lib/controller/controller.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/controller/controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+class Controllerclass extends ControllerMVC {
+  factory Controllerclass() => _this ??= Controllerclass._();
+  Controllerclass._();
+  static Controllerclass? _this;
+
+  void getstudent() {}
+}
